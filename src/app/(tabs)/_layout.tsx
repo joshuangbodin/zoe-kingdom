89b6e38fa5f8da0d9aa579f0e0e220109ef0c79d
Josh <ngbodinjoshua@gmail.com>
2026-05-21 21:21:21 +0100

    tabbar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,66 @@
 import { Tabs } from "expo-router";
+import { CheckSquare, Home, Newspaper, User } from "lucide-react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="habits" options={{ title: "Habits" }} />
-      <Tabs.Screen name="feed" options={{ title: "Feed" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+
+        tabBarStyle: {
+          backgroundColor: "#ffffff",
+          borderTopWidth: 1,
+          borderTopColor: "#e5e7eb",
+          height: insets.bottom + 60,
+          paddingTop: 8,
+          paddingBottom: insets.bottom + 10,
+        },
+
+      
+
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => <Home size={size} color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="habits"
+        options={{
+          title: "Habits",
+          tabBarIcon: ({ size, color }) => (
+            <CheckSquare size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="feed"
+        options={{
+          title: "Feed",
+          tabBarIcon: ({ size, color }) => (
+            <Newspaper size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ src/libs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHp_7W9TqwITZxFVRzhkSzqHxbQ4LFBpE",
+  authDomain: "zoe-kingdom.firebaseapp.com",
+  projectId: "zoe-kingdom",
+  storageBucket: "zoe-kingdom.firebasestorage.app",
+  messagingSenderId: "388516020259",
+  appId: "1:388516020259:web:6059ebb33543b337023196",
+  measurementId: "G-YVK8XDHS50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
