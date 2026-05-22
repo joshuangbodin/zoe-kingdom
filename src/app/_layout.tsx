0e0cb0da1de316a560bc433a6328e5578e0917dd
Josh <ngbodinjoshua@gmail.com>
2026-05-22 21:07:27 +0100

    done

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,8 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import "../global.css";
@@ -13,9 +15,33 @@ import { Stack } from "expo-router";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  // initialize sqlite database
   useEffect(() => {
     initDB();
   }, []);
+
+  
+  // load fonts
+  // const [loaded, error] = useFonts({
+  //   "Sora-Regular": require("@/assets/font/Sora/Sora-Regular.ttf"),
+  //   "Sora-Medium": require("@/assets/font/Sora/Sora-Medium.ttf"),
+  //   "Sora-SemiBold": require("@/assets/font/Sora/Sora-SemiBold.ttf"),
+  //   "Sora-Bold": require("@/assets/font/Sora/Sora-Bold.ttf"),
+
+  //   // serif
+  //   "Serif-Regular": require("@/assets/font/Serif/NotoSerif-Regular.ttf"),
+  //   "Serif-Italic": require("@/assets/font/Serif/NotoSerif-Italic.ttf"),
+  // });
+
+  // useEffect(() => {
+  //   if (loaded || error) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded, error]);
+
+  // if (!loaded && !error) {
+  //   return null;
+  // }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
